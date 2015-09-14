ce2ee4b58502f81c710e0e737e80a4613adda1fb
root <root@FortAwesomePi001.(none)>
2015-09-14 13:28:20 -0500

    first edit of EX001

---
+++ SuperKit_new/Ex001_ledBlink/fastled.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+
+LedPin = 11
+
+GPIO.setmode(GPIO.BOARD)	# Numbers GPIOs by physical location
+GPIO.setup(LedPin, GPIO.OUT)	# Set LedPin's MODE as OUT
+GPIO.output(LedPin, GPIO.HIGH)	# Set LedPin as HIGH +3.3v to turn LED off
+
+try:
+	while True:
+		print 'led ON'
+		GPIO.output(LedPin, GPIO.LOW)	# turn LED on
+		time.sleep(0.05)
+		print 'led OFF'
+		GPIO.output(LedPin, GPIO.HIGH)	# turn LED off
+		time.sleep(0.05)
+except KeyboardInterrupt:	# When CTRL-C is detected, the following
+				# code will be executed.
+	print 'CTRL-C detected. Cleaning up and exiting.'
+	GPIO.output(LedPin, GPIO.HIGH)	# turn LED off
+	GPIO.cleanup()			# Release resources
+
